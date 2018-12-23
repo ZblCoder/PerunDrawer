68547b6d4580bcdc29574a9f4c7a377eba482eca
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-12-23 17:33:34 +0300

    Added list demo

---
+++ Scripts/Demos/ListDemo.cs
@@ -0,0 +1,34 @@
+﻿using PerunDrawer;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListDemo : MonoBehaviour {
+    [Serializable]
+    public sealed class Weapon {
+        public string Name;
+        public int Damage;
+
+        public Weapon(string name, int damage) {
+            Name = name;
+            Damage = damage;
+        }
+    }
+
+    public List<Weapon> Weapons = new List<Weapon>();
+
+    [Button]
+    public void Clear() {
+        Weapons.Clear();
+    }
+
+    [Button("Add sword", Align = ButtonAttribute.AlignTypes.Bottom)]
+    public void AddSword() {
+        Weapons.Add(new Weapon("Sword", 10));
+    }
+
+    [Button("Add Pike", Align = ButtonAttribute.AlignTypes.Bottom)]
+    public void AddPike() {
+        Weapons.Add(new Weapon("Pike", 15));
+    }
+}
