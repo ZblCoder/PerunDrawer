9c7e31407444da95cabffaa683bbc4340ed3ff2c
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-12-23 17:41:08 +0300

    Order demo

---
+++ Scripts/Demos/OrderDemo.cs
@@ -0,0 +1,21 @@
+﻿using PerunDrawer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderDemo : MonoBehaviour {
+    [Order(3)]
+    public int FirstInCode = 1;
+
+    [Order(2)]
+    public int SecondInCode = 2;
+
+    [Order(1)]
+    public int ThirdInCode = 3;
+
+    [Order(0)]
+    public int FourthInCode = 4;
+
+    [Order(-1)]
+    public int FifthInCode = 5;
+}
